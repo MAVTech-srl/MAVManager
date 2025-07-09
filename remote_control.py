7a3d493f3829ef17d8171b8622535c35f9c834ae
DavideCarminati <davide.carminati@outlook.com>
2025-07-09 09:25:18 +0200

    Initial release. Bugged: child processes won't stop

---
+++ remote_control.py
@@ -0,0 +1,246 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input, State, DiskcacheManager, set_props
+import dash_daq as daq
+import plotly.express as px
+import subprocess
+import numpy as np
+import time
+import os
+import signal
+import psutil
+
+# Initialize the app
+external_stylesheets = ['/home/davide/Documents/open3d_devel/dash_assets/style.css']
+app = Dash(external_stylesheets=external_stylesheets, assets_folder="assets")
+# app = Dash()
+
+import diskcache
+cache = diskcache.Cache("./cache")
+background_callback_manager = DiskcacheManager(cache)
+
+'''
+Build top banner
+'''
+def build_banner():
+    return html.Div(
+        id="banner",
+        className="banner",
+        children=[
+            html.Div(
+                id="banner-text",
+                children=[
+                    html.H5("MAVTech Dashboard"),
+                    html.H6("Drone monitoring and logging"),
+                ],
+            ),
+            html.Button(className="banner button",
+                        id="help-button", children="HELP", n_clicks=0
+                    ),
+            html.Div(
+                # id="banner-logo",
+                className="banner logo",
+                children=[
+                    html.A(
+                        html.Img(id="logo", src=app.get_asset_url("mavtech.png")),
+                        href="https://www.mavtech.eu/it/",
+                    ),
+                ],
+            ),
+        ],
+    )
+
+
+# Check the tty device names
+bash_command = "ls -v /dev/ | awk '/^(ttyA|ttyU)/'" # List all files that begin with ttyA or ttyU
+subproc = subprocess.run(bash_command, shell=True, check=True, executable='/bin/bash', stdout=subprocess.PIPE)
+subproc_stdout = str(subproc.stdout.decode()).split()
+# App layout
+app.layout = [
+    # html.Div(className='row', children='Remote Control',
+    #                 style={'textAlign': 'center', 'color': 'red', 'fontSize': 30}),
+    build_banner(),
+    dcc.Tabs(className="custom tabs", children=[
+        dcc.Tab(label='Control', children=[   
+            html.Div(className="six columns", children=[
+                html.Div(className="row", children=[
+                    html.Br(),
+                    dcc.Markdown(''' 
+                                Select which UART port is used to communicate with the autopilot
+                                '''),
+                    dcc.Dropdown(subproc_stdout, id='tty-dropdown', placeholder='Select UART port...', clearable=True),
+                ]),
+                html.Div(className="row", id="lidar-model-selector", children=[
+                    html.Br(),
+                    dcc.Markdown('''Select LiDAR model'''),
+                    dcc.Dropdown(['AVIA', 'MID360'], id='lidar-model-dropdown', placeholder='Select model...'),
+                ]),
+                
+                html.Div(className="row", hidden=True, id="return-mode-selector", children=[
+                    html.Br(),
+                    dcc.Markdown('''Select LiDAR return mode'''),
+                    dcc.Dropdown(['First single', 'Strongest single', 'Dual'], id='return-mode-dropdown', placeholder='Select return mode...'),
+                ]),
+                html.Div(className="row", hidden=True, id="scan-pattern-selector", children=[
+                    html.Br(),
+                    dcc.Markdown('''Select LiDAR scan pattern'''),
+                    dcc.Dropdown(['Repetitive', 'Non-repetitive'], id='scan-pattern-dropdown', placeholder='Select scan pattern...'),
+                ]),
+                html.Br(),
+                html.Div(className="two columns", id='slam-starter-div', children=[
+                    dcc.ConfirmDialogProvider(
+                        children=html.Button(className='button hover', children=['Start SLAM'], style={'color': 'white', 'background': 'green', 'text-align': 'center'}),
+                        id='slam-starter',
+                        message='The UAV will start the SLAM process. Continue?'
+                    ),
+                ]),
+                html.Div(className="two columns", hidden=True, id='slam-stopper-div', children=[
+                    dcc.ConfirmDialogProvider(
+                        children=html.Button(className='button hover', children=['Stop SLAM'], style={'color': 'white', 'background': 'red'}),
+                        id='slam-stopper',
+                        message='Do you want to stop the SLAM process?'
+                    ), 
+                ]),
+                html.Div(id='button-div'),
+                html.Div(id="dummy-div")
+            ]),
+            html.Div(className="six columns", children=[
+                html.Br(),
+                dcc.Markdown(''' 
+                            Status
+                            '''),
+                # html.Div(className="row", id="status-terminal", children="")
+                dcc.Textarea(id="status-terminal", readOnly=True, style={'width': '100%', 'height': 200})
+            ]),
+        ]),
+        dcc.Tab(label='Logs', children=[
+            html.Div(className="six columns", children=[
+                html.Br(),
+                dcc.Markdown("SLAM logs"),
+                dcc.Textarea(id="slam-terminal", readOnly=True, style={'width': '100%', 
+                                                                        'height': 200}),
+                #
+                html.Br(),
+                dcc.Markdown("MAVROS logs"),
+                dcc.Textarea(id="mavros-terminal", readOnly=True, style={'width': '100%', 'height': 200})
+            ]),
+            html.Div(className="six columns", children=[
+                html.Br(),
+                dcc.Markdown("PTP logs"),
+                dcc.Textarea(id="ptp-terminal", readOnly=True, style={'width': '100%', 'height': 200})
+            ]),
+        ])
+    ])
+
+]
+
+###############################################################
+########################## CALLBACKS ##########################
+###############################################################
+
+# Select if AVIA or MID360
+@callback(Output(component_id='return-mode-selector', component_property='hidden'),
+          Output(component_id='scan-pattern-selector', component_property='hidden'),
+          Input(component_id='lidar-model-dropdown', component_property='value'),
+        #   prevent_initial_call=True
+          )
+def select_lidar_model(model):
+    if model == "AVIA":
+        return False, False
+    else:
+        return True, True
+
+slam_pid = 0
+mavros_pid = 0
+ptp_pid = 0
+slam_children = []
+
+# START SLAM callback
+@callback(
+    Output(component_id='button-div', component_property='children'),
+    Input(component_id='slam-starter', component_property='submit_n_clicks'),
+    State('return-mode-dropdown', 'value'),
+    State('scan-pattern-dropdown', 'value'),
+    State('tty-dropdown', 'value'),
+    State('lidar-model-dropdown', 'value'),
+    background=True,
+    running=[
+        (Output("slam-starter-div", "hidden"), True, False),
+        (Output("slam-stopper-div", "hidden"), False, True),
+    ],
+    cancel=[Input("slam-stopper", "submit_n_clicks")],
+    manager=background_callback_manager,
+    prevent_initial_call=True,
+    progress=[Output("ptp-terminal", "value"),
+            Output("mavros-terminal", "value"),
+            Output("slam-terminal", "value"),
+            Output("status-terminal", "value")]
+)
+def start_slam(set_progress, # This must be the first argument
+            submit_n_clicks: int,
+            return_mode: str,
+            scan_pattern: str,
+            tty: str,
+            model: str):
+    # User clicked "start SLAM"
+    status_msg = "SLAM is starting with the following configuration:\n -> lidar model: {}\n  -> tty: {}\n  -> return mode: {}\n  -> scan pattern: {}\n".format(model, tty, return_mode, scan_pattern)
+    # Retrieve bash scripts
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    slam_script_path = os.path.join(dir_path, "scripts/slam-ros2/start_docker_slam_avia.sh")
+    start_slam_cmd = "bash /home/davide/Documents/MAVManager/scripts/slam-ros2/fast_lio_slam/start_docker_slam_avia.sh"
+    start_mavros_cmd = ""
+    start_ptp_cmd = ""
+    slam_subprocess = subprocess.Popen(start_slam_cmd, stdout=subprocess.PIPE, shell=True)
+    mavros_subprocess = subprocess.Popen(start_mavros_cmd, stdout=subprocess.PIPE, shell=True)
+    ptp_subprocess = subprocess.Popen(start_ptp_cmd, stdout=subprocess.PIPE, shell=True)
+    os.set_blocking(slam_subprocess.stdout.fileno(), False)
+    os.set_blocking(mavros_subprocess.stdout.fileno(), False)
+    os.set_blocking(ptp_subprocess.stdout.fileno(), False)
+    time.sleep(2) # Wait for the processes to start!!
+
+    global slam_pid, mavros_pid, ptp_pid
+
+    slam_pid = slam_subprocess.pid
+    slam_parent = psutil.Process(slam_pid)
+
+    global slam_children
+    slam_children = slam_parent.children(recursive=True)
+    mavros_pid = mavros_subprocess.pid
+    ptp_pid = ptp_subprocess.pid
+
+
+    slam_line = ""
+    mavros_line = ""
+    ptp_line = ""
+    log_time = time.time_ns()
+    while True:
+        elapsed = f'{(time.time_ns() - log_time) / 10**9:.2f}'
+        #
+        slam_line = slam_subprocess.stdout.readline().decode() + slam_line
+        #
+        mavros_line = mavros_subprocess.stdout.readline().decode() + mavros_line
+        #
+        ptp_line = ptp_subprocess.stdout.readline().decode() + ptp_line
+        
+        set_progress((ptp_line, mavros_line, slam_line, status_msg))
+
+
+@callback(
+    Output(component_id='dummy-div', component_property='children'),
+    Input(component_id='slam-stopper', component_property='submit_n_clicks'),
+    prevent_initial_call=True
+)
+def stop_slam(submit_n_clicks):
+    stop_slam_processes()
+    
+
+def stop_slam_processes():
+    global slam_pid, mavros_pid, ptp_pid, slam_children
+    os.kill(slam_pid, signal.SIGKILL)
+    os.kill(mavros_pid, signal.SIGKILL)
+    os.kill(ptp_pid, signal.SIGKILL)
+    for pid in slam_children:
+        os.kill(pid, signal.SIGKILL)
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scripts/README.md
@@ -0,0 +1,14 @@
+# Script folder
+This folder should contain *as submodules* the bash scripts that start a particular feature/algorithm (e.g. SLAM). The folder structure should be the following:
+```bash
+scripts
+├── <submodule_a>
+│   ├── <algorithm_1>
+│   ├── ...
+│   └── <algorithm_n>
+├── <submodule_b>
+│   ├── <algorithm_1>
+│   ├── ...
+│   └── <algorithm_n>
+└── README.md           # <-- This README.md
+```
