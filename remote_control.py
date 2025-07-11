19f2a47fd538446c6682ec7707085174a77c76e5
sanemildizz <sanemildiz@gmail.com>
2025-07-11 11:10:40 +0200

    Now mavmanager edits lidar config files and it stops running containers

--- remote_control.py
+++ remote_control.py
@@ -1,4 +1,6 @@
 # Import packages
+from ast import mod
+from curses import baudrate
 from dash import Dash, html, dash_table, dcc, callback, Output, Input, State, DiskcacheManager, set_props
 import dash_daq as daq
 import plotly.express as px
@@ -8,6 +10,7 @@ import time
 import os
 import signal
 import psutil
+import json
 
 # Initialize the app
 external_stylesheets = ['/home/davide/Documents/open3d_devel/dash_assets/style.css']
@@ -53,6 +56,8 @@ def build_banner():
 bash_command = "ls -v /dev/ | awk '/^(ttyA|ttyU)/'" # List all files that begin with ttyA or ttyU
 subproc = subprocess.run(bash_command, shell=True, check=True, executable='/bin/bash', stdout=subprocess.PIPE)
 subproc_stdout = str(subproc.stdout.decode()).split()
+
+baudrate_list = [57600, 115200, 921600]
 # App layout
 app.layout = [
     build_banner(),
@@ -65,6 +70,13 @@ app.layout = [
                                 Select which UART port is used to communicate with the autopilot
                                 '''),
                     dcc.Dropdown(subproc_stdout, id='tty-dropdown', placeholder='Select UART port...', clearable=True),
+                ]),
+                html.Div(className="row", style={"padding": "10px"}, children=[
+                    html.Br(),
+                    dcc.Markdown(''' 
+                                Select UART baud rate
+                                '''),
+                    dcc.Dropdown(baudrate_list, id='baud-dropdown', placeholder='Select baud rate...', clearable=True),
                 ]),
                 html.Div(className="row", id="lidar-model-selector", style={"padding": "10px"},  children=[
                     html.Br(),
@@ -140,6 +152,38 @@ app.layout = [
 
 ]
 
+def update_lidar_config(params: dict):
+    # Lidar AVIA
+    mode = 0
+    pattern = 1
+    if params["model"] == 'AVIA':
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/config/livox_lidar_config.json") 
+        # print(path)
+        if params["return_mode"] == 'First single':
+            mode = 0
+        elif params["return_mode"] == 'Strongest single':
+            mode = 1
+        elif params["return_mode"] == 'Dual':
+            mode = 2
+        
+        if params["scan_pattern"] == 'Repetitive':
+            pattern = 1
+        else:
+            pattern = 0
+
+        with open(path, 'r') as f:
+            data = json.load(f)
+            
+            data['lidar_config'][0]['return_mode'] = mode
+            # print(mode)
+            data['lidar_config'][0]['scan_pattern'] = pattern
+            # print(pattern)
+
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2)
+
+
 ###############################################################
 ########################## CALLBACKS ##########################
 ###############################################################
@@ -168,6 +212,7 @@ slam_children = []
     State('return-mode-dropdown', 'value'),
     State('scan-pattern-dropdown', 'value'),
     State('tty-dropdown', 'value'),
+    State('baud-dropdown', 'value'),
     State('lidar-model-dropdown', 'value'),
     background=True,
     running=[
@@ -177,51 +222,57 @@ slam_children = []
     cancel=[Input("slam-stopper", "submit_n_clicks")],
     manager=background_callback_manager,
     prevent_initial_call=True,
-    progress=[Output("ptp-terminal", "value"),
-            Output("mavros-terminal", "value"),
+    progress=[Output("mavros-terminal", "value"),
             Output("slam-terminal", "value"),
-            Output("status-terminal", "value"),
-            Output("ptp-pid", "children")]
+            Output("status-terminal", "value")]
 )
 def start_slam(set_progress, # This must be the first argument
             submit_n_clicks: int,
             return_mode: str,
             scan_pattern: str,
             tty: str,
+            baud: int,
             model: str):
     # User clicked "start SLAM"
     status_msg = "The following configuration was selected:\n -> lidar model: {}\n  -> tty: {}\n  -> return mode: {}\n  -> scan pattern: {}\n".format(model, tty, return_mode, scan_pattern)
 
+    # Customize the lidar config files as per user choices
+    lidar_params = {"model": model,
+                    "scan_pattern": scan_pattern,
+                    "return_mode": return_mode
+                    }
+    update_lidar_config(lidar_params)
+
     # Retrieve bash scripts
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    slam_script_path = os.path.join(dir_path, "scripts/slam-ros2/start_docker_slam_avia.sh")
-    mavros_script_path = os.path.join(dir_path, "scripts/slam-ros2/start_mavros.sh")
-    ptp_script_path = os.path.join(dir_path, "scripts/slam-ros2/start_ptp.sh")
+    slam_script_path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/start_docker_slam_avia.sh")
+    mavros_script_path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/start_mavros.sh")
+    # ptp_script_path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/start_ptp.sh")
     start_slam_cmd = "bash " + slam_script_path
     start_mavros_cmd = "bash " + mavros_script_path
-    start_ptp_cmd = "bash " + ptp_script_path
+    # start_ptp_cmd = "bash " + ptp_script_path
 
     # Start the needed processes
     slam_subprocess = subprocess.Popen(start_slam_cmd, stdout=subprocess.PIPE, shell=True)
-    mavros_subprocess = subprocess.Popen(start_mavros_cmd, stdout=subprocess.PIPE, shell=True)
-    ptp_subprocess = subprocess.Popen(start_ptp_cmd, stdout=subprocess.PIPE, shell=True)
+    mavros_subprocess = subprocess.Popen(start_mavros_cmd + " " + tty + " " + str(baud) , stdout=subprocess.PIPE, shell=True)
+    # ptp_subprocess = subprocess.Popen(start_ptp_cmd, stdout=subprocess.PIPE, shell=True)
     os.set_blocking(slam_subprocess.stdout.fileno(), False)
     os.set_blocking(mavros_subprocess.stdout.fileno(), False)
-    os.set_blocking(ptp_subprocess.stdout.fileno(), False)
+    # os.set_blocking(ptp_subprocess.stdout.fileno(), False)
     time.sleep(2) # Wait for the processes to start!!
 
     global slam_pid, mavros_pid, ptp_pid
 
-    slam_pid = slam_subprocess.pid
-    slam_parent = psutil.Process(slam_pid)
+    # ptp_pid = ptp_subprocess.pid
+    # ptp_parent = psutil.Process(ptp_pid)
 
-    global slam_children
-    slam_children = slam_parent.children(recursive=True)
-    slam_children_pid = []
-    for pid in slam_children:
-        slam_children_pid.append(pid.pid)
-    mavros_pid = mavros_subprocess.pid
-    ptp_pid = ptp_subprocess.pid
+    # global slam_children
+    # ptp_children = ptp_parent.children(recursive=True)
+    # ptp_children_pid = []
+    # for pid in ptp_children:
+    #     ptp_children_pid.append(pid.pid)
+    # mavros_pid = mavros_subprocess.pid
+    # ptp_pid = ptp_subprocess.pid
 
 
     slam_line = ""
@@ -235,20 +286,19 @@ def start_slam(set_progress, # This must be the first argument
         #
         mavros_line = mavros_subprocess.stdout.readline().decode() + mavros_line
         #
-        ptp_line = ptp_subprocess.stdout.readline().decode() + ptp_line
+        # ptp_line = ptp_subprocess.stdout.readline().decode() + ptp_line
         
-        set_progress((ptp_line, mavros_line, slam_line, status_msg, ptp_pid))
+        set_progress((mavros_line, slam_line, status_msg))
 
 
 @callback(
     Output(component_id='dummy-div', component_property='children'),
     Input(component_id='slam-stopper', component_property='submit_n_clicks'),
-    State("ptp-pid", "children"),
     prevent_initial_call=True
 )
-def stop_slam(submit_n_clicks, child_pid):
-    subprocess.run("docker container stop fast-lio-slam", shell=True)
-    subprocess.run("docker container stop mavros", shell=True)
+def stop_slam(submit_n_clicks):
+    subprocess.run("docker container stop fast-lio-slam", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.run("docker container stop mavros", shell=True, stdout=subprocess.DEVNULL)
     # TODO: kill ptp process!! os.kill(ptp_pid, signal.SIGKILL)
     # subprocess.run()
 
