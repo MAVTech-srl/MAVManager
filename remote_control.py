1c6e8313632ebc3425fe2e77b0077c7d5ae9f192
DavideCarminati <davide.carminati@outlook.com>
2025-12-18 14:16:51 +0100

    Added a tab for editing fast-lio ROS parameters

--- remote_control.py
+++ remote_control.py
@@ -13,6 +13,8 @@ import psutil
 import json
 import zipfile
 import yaml
+import shutil
+import ast
 
 # Initialize the app
 external_stylesheets = ['/home/davide/Documents/open3d_devel/dash_assets/style.css']
@@ -140,17 +142,34 @@ app.layout = [
                     dcc.Checklist(['Save local point cloud'], id="pcd-checklist", value=['Save local point cloud']),
                     dcc.Checklist(['Save UTM (zone 32) point cloud'], id="utm-pcd-checklist"),
                     dcc.Checklist(['Convert livox custom cloud to ROS cloud'], id="convert-checklist"),
-                    dcc.Checklist(['Use the position covariance matrix from PX4 ekf2'], id="use-px4-cov-checklist"),
-                    html.Div(className='row', id='tune-ekf-text', children=[
-                        dcc.Markdown('Manually tune the noise covariance matrix R entries for x, y and z position:')
-                    ]),
-                    html.Span(children=[
-                        dcc.Input(id="input_cov_{}".format(_), type="text", value="0.1") for _ in ["x", "y", "z"]
-                    ], id='tune-ekf-span'),
+                    # dcc.Checklist(['Use the position covariance matrix from PX4 ekf2'], id="use-px4-cov-checklist"),
+                    # html.Div(className='row', id='tune-ekf-text', children=[
+                    #     dcc.Markdown('Manually tune the noise covariance matrix R entries for x, y and z position:')
+                    # ]),
+                    # html.Span(children=[
+                    #     dcc.Input(id="input_cov_{}".format(_), type="text", value="0.1") for _ in ["x", "y", "z"]
+                    # ], id='tune-ekf-span'),
                     html.Br(),
                     html.Br(),
                     html.Button(className='button', children=['Kill old slam session'], id='kill-old-session')
                 ])
+            ]),
+        ]),
+        dcc.Tab(label='SLAM parameters', children=[
+            html.Div(className="six columns", children=[
+                html.Span(id='button-span',
+                        children=[
+                            html.Button('Load YAML', id='load-button', style={'margin-top': '20px', 'margin-left': '20px'}),
+                            html.Button('Save YAML', id='save-button', style={'margin-top': '20px', 'margin-left': '20px'}),
+                            html.Button('Restore default', id='restore-button', style={'margin-top': '20px', 'margin-left': '20px'})
+                        ]),
+                html.Div(className='row', children=[
+                    dcc.Textarea(id="yaml-terminal", readOnly=True, draggable=False, style={'width': '100%', 'height': 5, 'padding': '10px', 'resize': 'vertical'}),
+                ], style={'padding': '10px'}),
+                html.Div(id='dictionary-display'),
+                dcc.Store(id='yaml-storage'),                       # Temporarily hold the dictionary
+                html.Div(id='yaml-path', hidden=True),              # Store the modified slam configuration path
+                html.Div(id='yaml-backup-path', hidden=True)        # Store the original config file to be restored if needed
             ]),
         ]),
         dcc.Tab(label='Logs', children=[
@@ -220,6 +239,10 @@ app.layout = [
 
 ]
 
+###############################################################
+####################### HELPER FUNCTIONS ######################
+###############################################################
+
 def update_lidar_config(params: dict):
     # Lidar AVIA
     mode = 0
@@ -251,22 +274,101 @@ def update_lidar_config(params: dict):
         with open(path, 'w') as f:
             json.dump(data, f, indent=2)
 
-def update_slam_config(params: dict):
-    # Avia
-    if params["model"] == 'AVIA':
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/config/avia.yaml") 
-        with open(path, 'r') as f:
-            slam_parameters = yaml.safe_load(f)
-            slam_parameters['/**']['ros__parameters']['mapping']['use_ekf2_covariance'] = params['use_px4_ekf2']
-            slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_x'] = params['cov_matrix'][0]
-            slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_y'] = params['cov_matrix'][1]
-            slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_z'] = params['cov_matrix'][2]
+# def update_slam_config(params: dict):
+#     # Avia
+#     if params["model"] == 'AVIA':
+#         dir_path = os.path.dirname(os.path.realpath(__file__))
+#         path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/config/avia.yaml") 
+#         with open(path, 'r') as f:
+#             slam_parameters = yaml.safe_load(f)
+#             slam_parameters['/**']['ros__parameters']['mapping']['use_ekf2_covariance'] = params['use_px4_ekf2']
+#             slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_x'] = params['cov_matrix'][0]
+#             slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_y'] = params['cov_matrix'][1]
+#             slam_parameters['/**']['ros__parameters']['mapping']['measurement_noise_covariance_z'] = params['cov_matrix'][2]
 
-        with open(path, 'w') as f:
-            yaml.safe_dump(slam_parameters, f, sort_keys=False, default_flow_style=False)
+#         with open(path, 'w') as f:
+#             yaml.safe_dump(slam_parameters, f, sort_keys=False, default_flow_style=False)
 
+def read_yaml_dict(dictionary: dict):
+    entries_body = []
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            entries_body.extend([html.H6("{}".format(key), style={"padding": "10px"})])
+            sub_div = read_yaml_dict(value)
+            entries_body.extend([html.Div(id='{}'.format(key), children=sub_div)])
+        else:
+            if isinstance(value, bool):
+                entries_body.extend([
+                    html.Div(children=[
+                        dcc.Markdown('{}'.format(key), style={
+                            "display": "inline-block",
+                            "width": "30%",
+                            "padding": "10px"
+                        }),
+                        dcc.Dropdown(id='bool-dropdown', options=['True', 'False'], value='True' if value is True else 'False', style={
+                            "display": "inline-block",
+                            "width": "30%",
+                            "verticalAlign": "top",
+                            "padding": "10px"
+                        })  # Using Dropdown as daq.BooleanSwitch sometimes defaults to true...
+                    ])
+                ])
+            else:
+                # Save the input type for when we'll save the yaml file. Lists must be converted into strings for dash as it supports only text or numbers.
+                # Lists will be restored using the ast library in the functio read_yaml_frontend
+                if isinstance(value, str):
+                    input_type = 'string'
+                elif isinstance(value, list):
+                    input_type = 'list'
+                else:
+                    input_type = 'float'
+                entries_body.extend([
+                    html.Div(children=[
+                        dcc.Markdown('{}'.format(key), style={
+                            "display": "inline-block",
+                            "width": "30%",
+                            "padding": "10px"
+                        }),
+                        dcc.Input(id=f'entry-{key}', type='text', value=str(value), placeholder=str(key), style={
+                            "display": "inline-block",
+                            "width": "30%",
+                            "padding": "10px"
+                        }) , 
+                        html.Div(id='input-type', children=f'{input_type}', hidden=True)
+                    ])
+                ])
+    return entries_body
 
+def read_yaml_frontend(frontend: list):
+    yaml_dict = {}
+    for idx, element in enumerate(frontend):
+        if element['type'] == 'H6':
+            # After a H6 there is always a Div that contains a list or another Div
+            key = element['props']['children']
+            dict_out = read_yaml_frontend(frontend[idx + 1]['props']['children'])
+            yaml_dict.update({key: dict_out})
+            continue
+            
+        if element['props']['children'][1]['type'] == 'Input':
+            entry = element['props']['children']
+            key = entry[0]['props']['children']
+            input_type = entry[2]['props']['children']
+            if input_type == 'string':
+                value = str(entry[1]['props']['value'])
+            elif input_type == 'list':
+                value = ast.literal_eval(entry[1]['props']['value'])
+            elif input_type == 'float':
+                value = float(entry[1]['props']['value'])
+            yaml_dict.update({key: value})
+
+        elif element['props']['children'][1]['type'] == 'Dropdown':
+            entry = element['props']['children']
+            key = entry[0]['props']['children']
+            value = True if entry[1]['props']['value'] == 'True' else False
+            yaml_dict.update({key: value})
+
+    return yaml_dict
+    
 
 ###############################################################
 ########################## CALLBACKS ##########################
@@ -289,18 +391,89 @@ def select_lidar_model(model):
     else:
         return True, True
 
+# @callback(
+#         Output('tune-ekf-text', 'hidden'),
+#         Output('tune-ekf-span', 'hidden'),
+#         Input(component_id='use-px4-cov-checklist', component_property='value'),
+#         prevent_initial_call=True
+# )
+# def toggle_tune_slam_ekf(chklst_value):
+#     if chklst_value and chklst_value[0] == 'Use the position covariance matrix from PX4 ekf2':
+#         return True, True
+#     else:
+#         return False, False
+
+# =============================================================
+# =====================| SLAM PARAMETERS |=====================
+# =============================================================
+
 @callback(
-        Output('tune-ekf-text', 'hidden'),
-        Output('tune-ekf-span', 'hidden'),
-        Input(component_id='use-px4-cov-checklist', component_property='value'),
+    Output('dictionary-display', 'children', allow_duplicate=True),
+    Output('yaml-storage', 'data', allow_duplicate=True),
+    Output('yaml-path', 'children'),
+    Output('yaml-backup-path', 'children'),
+    Output('yaml-terminal', 'value', allow_duplicate=True),
+    Input('load-button', 'n_clicks'),
+    State(component_id='lidar-model-dropdown', component_property='value'),
+    prevent_initial_call=True
+)
+def load_yaml(n_clicks, lidar_model):
+    if not lidar_model:
+        return [], {}, None, None, f'No Lidar model is selected. Select the model before loading its configuration.'
+    try:
+        if lidar_model == 'AVIA':
+            filename = 'avia.yaml'
+        elif lidar_model == 'MID360':
+            filename = 'mid360.yaml'
+        # Set corresponding config file path
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/config/", filename)
+        # Create a backup copy of the file
+        filename_backup = filename + '.backup'
+        backup_file_path = os.path.join(dir_path, "scripts/slam-ros2/fast_lio_slam/config/", filename_backup)
+        shutil.copy2(file_path, backup_file_path)
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+        display_entries = read_yaml_dict(data)
+        return display_entries, data, file_path, backup_file_path, 'File loaded correctly: {}'.format(file_path)
+    except Exception as e:
+        return [], {}, None, None, f'Error loading file: {e}'
+
+@callback(
+    Output('yaml-terminal', 'value', allow_duplicate=True),
+    Input('save-button', 'n_clicks'),
+    State('yaml-storage', 'data'),
+    State('yaml-path', 'children'),
+    State('dictionary-display', 'children'),
+    prevent_initial_call=True
+)
+def save_yaml(n_clicks, data, file_path, updated_frontend):
+    if not file_path:
+        return 'No configuration loaded! Load a valid configuration before saving.'
+    try:
+        # Fetch the data from frontend which is updated by the user
+        new_data = read_yaml_frontend(updated_frontend)
+        with open(file_path, 'w') as file:
+            yaml.dump(new_data, file)
+        return f'File saved successfully at {file_path}'
+    except Exception as e:
+        return f'Error saving file: {e}'
+
+@callback(
+        Output('dictionary-display', 'children', allow_duplicate=True),
+        Output('yaml-storage', 'data', allow_duplicate=True),
+        Output('yaml-terminal', 'value', allow_duplicate=True),
+        Input('restore-button', 'n_clicks'),
+        State('yaml-backup-path', 'children'),
         prevent_initial_call=True
 )
-def toggle_tune_slam_ekf(chklst_value):
-    if chklst_value and chklst_value[0] == 'Use the position covariance matrix from PX4 ekf2':
-        return True, True
-    else:
-        return False, False
-
+def restore_default_values(n_clicks, backup_file_path):
+    if not backup_file_path:
+        return [], {}, 'No default yaml file exist!'
+    with open(backup_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        display_entries = read_yaml_dict(data)
+        return display_entries, data, 'Successfully loaded default values!'
 
 # =============================================================
 # ======================| FILE BROWSER |=======================
@@ -388,8 +561,8 @@ def refresh_files(n_clicks):
     State('pcd-checklist', 'value'),
     State('utm-pcd-checklist', 'value'),
     State('convert-checklist', 'value'),
-    State('use-px4-cov-checklist', 'value'),
-    [State("input_cov_{}".format(_), 'value') for _ in ['x', 'y', 'z']],
+    # State('use-px4-cov-checklist', 'value'),
+    # [State("input_cov_{}".format(_), 'value') for _ in ['x', 'y', 'z']],
     background=True,
     running=[
         (Output("slam-starter-div", "hidden"), True, True),
@@ -414,8 +587,9 @@ def start_slam(set_progress, # This must be the first argument
             save_local: list,
             save_utm: list,
             convert_pc: list,
-            use_ekf2_cov: list,
-            *covariance: float):
+            # use_ekf2_cov: list,
+            # *covariance: float
+            ):
     # User clicked "start SLAM"
     # Check if all the fields were correctly filled
     if tty == None or baud == None or model == None:
@@ -429,15 +603,15 @@ def start_slam(set_progress, # This must be the first argument
                     "return_mode": return_mode
                     }
     update_lidar_config(lidar_params)
-    if use_ekf2_cov and use_ekf2_cov[0] == 'Use the position covariance matrix from PX4 ekf2':
-        cov_matrix = []
-        [cov_matrix.append(float(cov)) for cov in covariance if cov]
-        slam_params = {
-            'model': model,
-            'cov_matrix': cov_matrix,
-            'use_px4_ekf2': True
-        }
-        update_slam_config(slam_params)
+    # if use_ekf2_cov and use_ekf2_cov[0] == 'Use the position covariance matrix from PX4 ekf2':
+    #     cov_matrix = []
+    #     [cov_matrix.append(float(cov)) for cov in covariance if cov]
+    #     slam_params = {
+    #         'model': model,
+    #         'cov_matrix': cov_matrix,
+    #         'use_px4_ekf2': True
+    #     }
+    #     update_slam_config(slam_params)
 
     # Retrieve bash scripts
     dir_path = os.path.dirname(os.path.realpath(__file__))
